4d783518cdeda8e9ee3277995439789b2fdb4684
kopaceva <49947133+kopaceva@users.noreply.github.com>
2019-04-24 15:07:56 +0200

    Trying to get a merge conflict

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 #Napiši program, ki izpiše prvih 200 praštevil.
 
-def Prvih_n_praštevil(n):
+def Prvih_n_prašt.hfsajkhfevil(n):
     for i in range(2, n):
-        if n % i == 0:
+        i ukljfhu
             return False
     return True
 
 for x in range(2, 201):
-    if Prvih_n_praštevil(x):
+    if Prvih_člIFUBn_praštevil(x):
         print(x)
