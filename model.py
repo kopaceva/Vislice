9b9167e27852f4ff84d8fb7344856618200f9812
Eva Kopač <eva.kopac@student.fmf.uni-lj.si>
2019-05-15 15:47:17 +0200

    Končan model za vislice

--- model.py
+++ model.py
@@ -1,3 +1,5 @@
+import random
+
 STEVILO_DOVOLJENIH_NAPAK = 10
 PRAVILNA_CRKA = '+'
 PONOVLJENA_NAPAKA = 'o'
@@ -60,20 +62,27 @@ class Igra:
             else:
                 return NAPACNA_CRKA
 
+with open('u:\\Uvod v programiranje\\Repozitorij\\Vislice\\besede.txt', 'r', encoding = 'utf-8') as datoteka_z_besedami:
+    bazen_besed = [vrstica.strip().upper() for vrstica in datoteka_z_besedami]
 
-testno_geslo = 'požrtvovalnost'.upper()
-testne_crke = ['A', 'E', 'O', 'P']
-zmagovalne_crke = [x for x in testno_geslo]
-igra = Igra(testno_geslo, testne_crke)
-print(igra.napacne_crke())
-print(igra.pravilne_crke())
-print(igra.stevilo_napak())
-print(igra.zmaga())
-zmagana_igra = Igra(testno_geslo, zmagovalne_crke)
-print(zmagana_igra.zmaga())
-print(igra.poraz())
-print(igra.pravilni_del_gesla())
-print(igra.nepravilni_ugibi())
-poskus = igra.ugibaj('r')
-print(poskus)
-print(igra.pravilni_del_gesla())
+def nova_igra():
+    return Igra(random.choice(bazen_besed))
+
+#print(bazen_besed[:5])
+
+#testno_geslo = 'požrtvovalnost'.upper()
+#testne_crke = ['A', 'E', 'O', 'P']
+#zmagovalne_crke = [x for x in testno_geslo]
+#igra = Igra(testno_geslo, testne_crke)
+#print(igra.napacne_crke())
+#print(igra.pravilne_crke())
+#print(igra.stevilo_napak())
+#print(igra.zmaga())
+#zmagana_igra = Igra(testno_geslo, zmagovalne_crke)
+#print(zmagana_igra.zmaga())
+#print(igra.poraz())
+#print(igra.pravilni_del_gesla())
+#print(igra.nepravilni_ugibi())
+#poskus = igra.ugibaj('r')
+#print(poskus)
+#print(igra.pravilni_del_gesla())
