25c04d13d3fc8e794e2ad1b9984e40cfaa323a16
Eva Kopač <eva.kopac@student.fmf.uni-lj.si>
2019-05-22 16:59:28 +0200

    Slikce

--- datoteke/views/igra.tpl
+++ datoteke/views/igra.tpl
@@ -13,10 +13,30 @@
 
   <h2>{{igra.pravilni_del_gesla()}}</h2>
 
-  Nepravilne črke : {{igra.nepravilni_ugibi()}}
+  Nepravilne črke : {{igra.nepravilni_ugibi()}} 
 
   % preostali_poskusi = model.STEVILO_DOVOLJENIH_NAPAK - igra.stevilo_napak() + 1
-  Število preostalih poskusov: {{preostali_poskusi}}
-</body>
+  <br>Število preostalih poskusov: {{preostali_poskusi}}
+  <img src="/img/{{igra.stevilo_napak()}}.jpg" alt="obesanje">
 
+% if poskus == model.ZMAGA:
+<h2>ZMAGA!</h2>
+
+% elif poskus == model.PORAZ:
+
+<h2>Poraz!</h2>
+Pravilno geslo je {{igra.geslo}}.
+
+<form action="/igra/" method="post">
+    <button type="submit">Nova igra</button>
+</form>
+
+% else:
+<form action="/igra/{{id_igre}}/" method="post">
+    črka: <input type="text" name = "crka">
+    <button type="submit">Pošlji ugib</button>
+</form>
+% end
+
+</body>
 </html>
--- vislice.py
+++ vislice.py
@@ -11,8 +11,27 @@ vislice.ugibaj(id_testne_igre, 'A')
 def index():
     return bottle.template('index.tpl')
 
-@bottle.get('/igra')
-def testna_igra():
-    return bottle.template('igra.tpl', igra = vislice.igre[id_testne_igre][0], id_igre = id_testne_igre, poskus = vislice.igre[id_testne_igre][1])
+#@bottle.get('/igra')
+#def testna_igra():
+#    return bottle.template('igra.tpl', igra = vislice.igre[id_testne_igre][0], id_igre = id_testne_igre, poskus = vislice.igre[id_testne_igre][1])
+
+@bottle.post('/igra/')
+def nova_igra():
+    id_igre = vislice.nova_igra()
+    bottle.redirect('/igra/{0}/'.format(id_igre))
+
+@bottle.get('/igra/<id_igre:int>/')
+def pokazi_igro(id_igre):
+    return bottle.template('igra.tpl', igra = vislice.igre[id_igre][0], id_igre = id_igre, poskus = vislice.igre[id_igre][1])
+
+@bottle.post('/igra/<id_igre:int>/')
+def ugibaj(id_igre):
+    crka_za_ugib = bottle.request.forms.getunicode("crka")
+    vislice.ugibaj(id_igre, crka_za_ugib)
+    bottle.redirect('/igra/{0}/'.format(id_igre))
+
+@bottle.get('/img/<picture>')
+def serve_pictures(picture):
+    return bottle.static_file(picture, root = 'img')
 
 bottle.run(reloader=True, debug=True)
